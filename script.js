bdaf5c4d156f54b04688d2070e9b08577ac400ba
Aziz <aziz.amadaliev@gmail.com>
2021-12-18 12:53:41 -0500

    func other options added

--- script.js
+++ script.js
@@ -5,25 +5,90 @@ const increaseBtn = document.querySelector(".increase")
 
 let countNum = 0
 
-/* Func decrease */
-decreaseBtn.addEventListener('click', function(){
+/* Option 1 */
+
+//Func decrease
+/* decreaseBtn.addEventListener('click', function(){
     countNum--
     setColorAndValue()
 })
 
-/* Func reset */
+//Func reset
 resetBtn.addEventListener('click', function(){
     countNum = 0
     setColorAndValue()
 })
 
-/* Func increase */
+//Func increase
 increaseBtn.addEventListener('click', function(){
     countNum++
     setColorAndValue()
 })
+ */
 
-//func setting color and value
+
+
+/* ----------Option 2 with Foor loop------------*/
+
+const allBtns = document.querySelector(".btn") //brings array-like NodeList allBtns.length => 3
+//const allBtns = [{decreaseBtn}, {resetBtn}, {increaseBtn}]
+console.log(allBtns.length)
+
+for (let i = 0; i < allBtns.length; i++) {
+    allBtns[i].addEventListener("click", function(event){
+        console.log(event.target, "this is event target" )
+        const classes = event.target.classList;
+        console.log(classes, 'these are classes')
+        if (classes.contains("decrease")) {
+            countNum--
+        } else if (classes.contains("increase")) {
+            countNum++
+        } else {
+            countNum = 0;
+        }
+        setColorAndValue()
+    })
+}
+
+/* ----------Option 3------------ */
+/* allBtns.forEach(function(el){
+    el.addEventListener("click", function(event){
+        console.log(event.target, "this is event target" )
+        const classes = event.target.classList;
+        console.log(classes, 'these are classes')
+        if (classes.contains("decrease")) {
+            countNum--
+        } else if (classes.contains("increase")) {
+            countNum++
+        } else {
+            countNum = 0;
+        }
+        setColorAndValue()
+    })    
+}) */
+
+/* -------------Option 4--------------- */
+
+//allBtns.forEach(addEventToBtn) //high order method that under hood looping through array
+/* 
+function addEventToBtn(el) {
+    el.addEventListener("click", function(event){
+        //console.log(event.target, "this is event target" )
+        const classes = event.target.classList;
+        //console.log(classes, 'these are classes')
+        if (classes.contains("decrease")) {
+            countNum--
+        } else if (classes.contains("increase")) {
+            countNum++
+        } else {
+            countNum = 0;
+        }
+        setColorAndValue()
+    })
+
+} */
+
+// out sourc global func setting color and value
 function setColorAndValue () {
     if (countNum < 0) {
         span.style.color = 'red'; 
@@ -34,3 +99,4 @@ function setColorAndValue () {
     }
     span.innerText = countNum
 }
+// event is an object
